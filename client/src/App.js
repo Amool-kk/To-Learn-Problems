17809d82ade0556987c65789123fe42cf2b68557
iceonGit <icey58506@gmail.com>
2022-10-11 13:46:23 +0900

    Added HomePage and Navbar to the FrontEnd

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./pages/Home/Home";
+import Articles from "./pages/Articles/Articles";
 import "./App.css";
 
-function App() {
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Frontend setup</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/articles" element={<Articles />} />
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
