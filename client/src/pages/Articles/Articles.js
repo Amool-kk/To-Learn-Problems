b089605c273f5bf4e95bd971dc76fdacaabdf0c0
iceonGit <icey58506@gmail.com>
2022-10-11 15:43:47 +0900

    Added articles and cards

--- client/src/pages/Articles/Articles.js
+++ client/src/pages/Articles/Articles.js
@@ -1,7 +1,36 @@
 import React from "react";
+import "./Articles.css";
 
-const Articles = () => {
-  return <div>Articles</div>;
+const Articles = (props) => {
+  return (
+    <div class="container">
+      <div class="cards">
+        <div class="card card-one">
+          <h2 class="card-title">Anxieties</h2>
+          <p class="date">October 11,2022</p>
+          <p class="description">Nothing to worry about</p>
+        </div>
+
+        <div class="card card-two">
+          <h2 class="card-title">Old News</h2>
+          <p class="date">October 10,2022</p>
+          <p class="description">Boom boom Becker strikes again.</p>
+        </div>
+
+        <div class="card card-three">
+          <h2 class="card-title">Heavy</h2>
+          <p class="date">October 5,2022</p>
+          <p class="description">So how you like it now ?</p>
+        </div>
+
+        <div class="card card-four">
+          <h2 class="card-title">San Andreas</h2>
+          <p class="date">October 12,2022</p>
+          <p class="description">Here we go again.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Articles;
